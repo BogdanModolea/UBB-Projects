547fc1f82f2f7adcbfeb04c87e430a0d98217ca8
BogdanModolea <bogdanmodolea30@gmail.com>
2023-06-27 22:43:20 +0300

    Added SDI A1

--- Semester 4/Systems for Design and Implementation/Exam/backend/Exam/src/main/java/com/example/exam/repository/ExaminationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.exam.repository;
-
-import com.example.exam.entity.Examination;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExaminationRepository extends JpaRepository<Examination, Integer> {
-
-}
--- Semester 4/Systems for Design and Implementation/Exam/backend/Exam/src/main/java/com/example/exam/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.exam.repository;
-
-import com.example.exam.entity.Examinator;
-import com.example.exam.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
